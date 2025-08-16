dbac9b4f59948f6c8c1a8e7d43aba0961936b888
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-16 12:40:35 +0530

    Started the luv cpp

---
+++ Basic.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout<< "Hello World"<<" " << "My name is Aditya Kumar"<<endl;
+	int a = 2;
+	int b = 3;
+	int sum = a+b;
+	char c = 'a';
+	cout<<"The value of c is "<<c << endl;
+
+	double z = 2983.28389;
+	a = 1.6;
+	cout<<c << " "<<z<<" "<<" "<< a;
+
+}
