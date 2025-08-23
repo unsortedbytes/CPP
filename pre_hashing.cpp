a5c9dc160e6d3e77b85cdf9fa454d191ac370570
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-23 23:59:21 +0530

    Hashing

--- pre_hashing.cpp
+++ pre_hashing.cpp
@@ -1,23 +1,46 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+// 	int n;
+// 	cin>>n;
+// 	int a[n];
+// 	for(int i=0;i<n;i++){
+// 		cin>>a[i];
+// 	}
+// 	int q;
+// 	cin>>q;
+// 	while(q){
+// 		int x;
+// 		cin>>x;
+// 		int ct = 0;
+// 		for(int i=0;i<n;i++){
+// 			if(x == a[i]) ct++;
+// 		}
+// 		cout<<ct<<endl;
+// 		q--;
+// 	}
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
+
+const int N = 1e7+10;
+int hsh[N];
 
 int main(){
 	int n;
 	cin>>n;
 	int a[n];
-	for(int i=0;i<n;i++){
+	for(int i = 0;i<n;i++){
 		cin>>a[i];
+		hsh[a[i]]++;
 	}
 	int q;
 	cin>>q;
-	while(q){
+	while(q--){
 		int x;
 		cin>>x;
-		int ct = 0;
-		for(int i=0;i<n;i++){
-			if(x == a[i]) ct++;
-		}
-		cout<<ct<<endl;
-		q--;
+		cout<<hsh[x]<<endl;	
 	}
 }
