236118054a7644edbd630b25c45384ac6db46dca
unsortedkgpian <theadityakumar2810@gmail.com>
2025-09-09 01:01:33 +0530

    upper_bound and lower_bound

---
+++ upperboud_and_lowerboud.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	sort(a, a+n);
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+	int *ptrl = lower_bound(a, a+n,4);
+	int *ptru = upper_bound(a, a+n , 25);// if greatest -> garbage value
+	cout<<*ptrl<<endl;
+	cout<<*ptru<<endl;
+
+	if(ptrl == (a+n)){
+		cout<<"NOT FOUND";
+	}
+	cout<<(*ptrl)<<endl;
+
+
+	set<int> s;
+	for(int i=0;i<n;++i){
+		int x;
+		cin>>x;
+		s.insert(x);
+	}
+	// auto it = lower_bound(s.begin(), s.end(), 25);
+	auto it = s.lower_bound(23);
+	cout<<(*it)<<endl;
+}
