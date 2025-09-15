ce05ef379d1173d439e97c0aab8e6740e889ada3
unsortedkgpian <theadityakumar2810@gmail.com>
2025-09-15 15:28:34 +0530

    1399A

---
+++ 1399A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+       	sort(arr.begin() , arr.end());
+        bool check = true;
+        for (int i = 0; i < n - 1; i++) {
+            if (abs(arr[i] - arr[i + 1]) > 1) {
+                check = false;
+                break;
+            }
+        }
+
+        if (check)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
