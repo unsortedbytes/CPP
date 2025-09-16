db33fb9f47dbb27b2e8e7979e1a7c3739becb7a9
unsortedkgpian <theadityakumar2810@gmail.com>
2025-09-16 11:05:13 +0530

    1899A

---
+++ 1899A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int x;
+		cin>>x;
+		if(x%3!=0) {
+			cout<<"First"<<endl;
+		}else{
+			cout<<"Second"<<endl;
+		}
+	}	
+}
