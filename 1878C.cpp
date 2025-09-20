d4116bbe160beb659b175efda4072b39be82d6da
unsortedkgpian <theadityakumar2810@gmail.com>
2025-09-20 14:05:28 +0530

    1878C

---
+++ 1878C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long int n, k,x;
+		cin>>n>>k>>x;
+		long long lower = k*(k+1)/2;
+		long long upper = k*((2*n) -k +1)/2;
+		if(x>=lower && x<=upper){
+			cout<<"YES"<<endl;
+		}else{
+			cout<<"NO"<<endl;
+		}
+
+	}
+}
