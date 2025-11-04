e4c14f2bd4e55640efaada3147416f140b106410
unsortedkgpian <theadityakumar2810@gmail.com>
2025-11-04 10:09:44 +0530

    1559A

--- 1559A.cpp
+++ 1559A.cpp
@@ -8,17 +8,23 @@ int main(){
 		int n;
 		cin>>n;
 		int arr[n];
-		int ecnt =0, ocnt=0;
+
+		// int ecnt =0, ocnt=0;
 		for(int i=0;i<n;i++){
 			cin>>arr[i];
-			if(ecnt%2==0){
-				ecnt++;
-			}else{
-				ocnt++;
-			}
+			// if(ecnt%2==0){
+			// 	ecnt++;
+			// }else{
+			// 	ocnt++;
+			// }
 		}
+		int bit=arr[0];
+		for(int i=1;i<n;i++){
+			bit = bit&arr[i];
+		}
+		cout<<bit<<endl;
 
-		
+
 
 	}
 }
